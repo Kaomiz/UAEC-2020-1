bf6052dfa3d1129a5cfe58e62098e407f3bf5386
ethan <ethan.pretula@gmail.com>
2020-10-25 12:00:07 -0600

    Read only???????????

--- app/src/main/java/com/example/uaec_2020/MainActivity.java
+++ app/src/main/java/com/example/uaec_2020/MainActivity.java
@@ -11,7 +11,10 @@ import android.widget.ImageView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
 		ImageView imageView = new ImageView (null);
 		imageView.setImageBitmap (image);*/
 		UserShit user = new UserShit ();
-		JSONObject person =
+		JSONObject person = new JSONObject ();
+		try {
+			person = user.createJSONObject ("Sam", "Tait", "Yeety", "Yoink","yikes","T6A3C9","78069696969","42069","Death");
+		} catch (JSONException e) {
+			e.printStackTrace ();
+		}
+		try {
+			user.storeJSONObject (person);
+		} catch (IOException e) {
+			e.printStackTrace ();
+		}
 	}
 }
